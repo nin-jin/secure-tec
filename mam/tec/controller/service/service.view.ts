e5c9449b88bec8cf732e27493dcc6e10006e646d
jin <jin@hyoo.ru>
2023-04-20 23:43:31 +0300

    Mem contoller key in session

--- mam/tec/controller/service/service.view.ts
+++ mam/tec/controller/service/service.view.ts
@@ -3,7 +3,14 @@ namespace $.$$ {
 		
 		@ $mol_mem
 		Peer() {
-			return $mol_wire_sync( $hyoo_crowd_peer ).generate()
+			
+			let serial = $mol_state_session.value( `${this}.Peer` ) as string | null
+			if( serial ) return $mol_wire_sync( $hyoo_crowd_peer ).restore( serial )
+			
+			const peer = $mol_wire_sync( $hyoo_crowd_peer ).generate()
+			$mol_state_session.value( `${this}.Peer`, peer.key_private_serial )
+			
+			return peer
 		}
 		
 		@ $mol_mem
