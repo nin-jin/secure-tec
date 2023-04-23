5c764795fc3ba7d3d7abac95e7f7cd7e0e5754fa
koplenov <koplenov@gmail.com>
2023-04-23 19:30:56 +0300

    default $tec_domain.id

--- tec/vendor/license/license.view.ts
+++ tec/vendor/license/license.view.ts
@@ -1,6 +1,12 @@
 namespace $.$$ {
 	export class $tec_vendor_license extends $.$tec_vendor_license {
 
+		@ $mol_mem
+		domain_id(val?: string) {
+			if ( val !== undefined ) return val as string
+			return $tec_domain.id
+		}
+
 		@$mol_action
 		bundle_license() {
 			const land = this.vendor().land
